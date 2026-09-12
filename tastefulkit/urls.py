b3b39e5cdecdf4a0e7d694592a04dd2c58e46c9e
Greg <greg@gregagi.com>
2026-09-12 11:28:33 +0000

    fix: constrain mobile design cards and serve local preview media

--- tastefulkit/urls.py
+++ tastefulkit/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
@@ -58,3 +59,6 @@ urlpatterns += [
         name="django.contrib.sitemaps.views.sitemap",
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
